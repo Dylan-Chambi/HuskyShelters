3e31ccfcfedce4cab1af332ba4c8353a3e704c12
Dylan_Chambi <dylanchambifi@gmail.com>
2022-06-15 17:14:30 -0400

    Update implementation

--- CloudFormation/src/app.py
+++ CloudFormation/src/app.py
@@ -1,4 +1,26 @@
+import boto3
+import json
+
+table = boto3.resource('dynamodb').Table('husky_shelter')
+
 def update_item(event, context):
+    
+    body = json.loads(event["body"])
+    print(body)
+    
+    table.update_item(
+        Key={'id': body["id"]},
+        AttributeUpdates={
+            "age": body["age"],
+            "collectionOfImages": body["collectionOfImages"],
+            "health": body["health"],
+            "location": body["location"],
+            "name": body["name"],
+            "status": body["status"]
+        },
+    )
+    
+
     return {
         'statusCode': 200,
         'body': "API test"
