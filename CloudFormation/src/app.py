1d930d27f35383a7d36d156647ab37341974cae9
Marcos Simon <marcosht503@gmail.com>
2022-06-17 18:37:42 -0400

    Added .csv implementation for table elements

--- CloudFormation/src/app.py
+++ CloudFormation/src/app.py
@@ -1,5 +1,72 @@
+import json
+import csv
+import boto3
+
+s3 = boto3.client('s3')
+dynamodb = boto3.client('dynamodb', region_name = 'us-east-1')
+
+
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': "Hello World"
     }
+
+def excel_processing_handler(event, context):
+    animal_data = []
+    region = "us-east-1"
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+
+    print('Bucket: ', bucket, 'Key: ', key)
+
+    csv_file = s3.get_object(Bucket=bucket, Key=key)
+
+    animal_data = csv_file['Body'].read().decode('utf-8').split('\n')
+    csv_reader = csv.reader(animal_data, delimiter=',', quotechar='"')
+
+   
+
+    
+   
+    for row in csv_reader:
+        print(row)
+
+        if row: 
+            id = row[0]
+            name = row[1]
+            health = row[2]
+            status = row[3]
+            location = row[4]
+            images = row[5]
+            type = row[6]
+
+
+        add_to_dynamo = dynamodb.put_item(
+
+            TableName='husky_shelter',
+            Item={
+
+                'id' : {'S': id},
+                'name' : {'S': name},
+                'health' : {'S': health},
+                'status' : {'S': status},
+                'location' : {'S': location},
+                'images' : {'S': images},
+                'type' : {'S': type}
+            
+            }
+
+
+    )    
+
+
+    return {
+        'statusCode': 200,
+        'body': "Hello World"
+    }
+
+
+
+
+
